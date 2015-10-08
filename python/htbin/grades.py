6532f657a6cf2c8353a6183b90ffd9d2b942b63e
jvansteeter <spam.magnet1991@gmail.com>
2015-10-07 22:19:09 -0600

    started work python server

---
+++ python/htbin/grades.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+print "Content-type: text/html"
+print
+print "<h1>Grades</h1>"
+with open("grades.txt", "r") as grades:
+	while(True):
+		line = grades.readline()
+		if(line[0] == "#"):
+			print "<h2>" + line[1:len(line)] + "</h2><br>"
+		elif(line == ""):
+			break
+		else:
+			print line + "<br>"
---
+++ python/htbin/news.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+print "Content-type: text/html"
+print
+print "<h1>News</h1>"
